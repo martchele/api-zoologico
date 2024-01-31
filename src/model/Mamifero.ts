5e7fac61e2bcbe33c5ba5be8491d4e0bc74f95ee
martchele <126262772+martchele@users.noreply.github.com>
2024-01-31 16:00:25 -0300

    iniciando o projeto

---
+++ src/model/Mamifero.ts
@@ -0,0 +1,19 @@
+class Maminfero extends Animal {
+    private raca: string;
+
+    constructor(_raca: string,
+               _nome: string,
+               _idade: number,
+               _genero: string) {
+        super(_nome, _idade, _genero);
+        this.raca = _raca;
+    }
+         
+    public getRaca():  string{
+        return this.raca;
+    }
+    
+    public setRaca(raca:string): void{
+        this.raca;
+    }
+}
